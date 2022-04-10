6131f14e0b2181f713fc81399c0b2bed8f8dbfd1
erlendgit <62283820+erlendgit@users.noreply.github.com>
2022-04-10 15:04:17 +0200

    Fix testing (#2)
    * Fix the test.

--- project/home/tests.py
+++ project/home/tests.py
@@ -1,8 +1,29 @@
+from unittest import mock
+from unittest.mock import Mock
+
 from django.test import TestCase
 
-# Create your tests here.
+
 class HomeTestCase(TestCase):
 
-    def test_home_response(self):
+    @mock.patch('home.tasks.run_task.delay')
+    def test_home_response(self, mocked_run_task):
+        # Given.
+        task_response = Mock()
+        task_response.get.return_value = 'Hey! You!'
+        mocked_run_task.return_value = task_response
+
+        # When.
         response = self.client.get("/")
-        self.assertEqual(response.json(), {"msg": "Home is where your heart is."})
+
+        # Then.
+        self.assertEqual(response.json(), {"msg": "Home is where your heart is.",
+                                           "response": "Hey! You!"})
+        mocked_run_task.assert_called()
+        task_response.get.assert_called()
+
+        # @NOTE: I tested if the response is ok, and if testing worked.
+        # The last two assertions tell about the limit of unit testing:
+        #   they don't proof that the system as a whole works.
+        # Functional testing with selenium or at least manual testing
+        #   remains relevant.
