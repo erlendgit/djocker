35ad8f0f0fb6ff112fcf63929c680a1c2114744a
Erlend ter Maat <erlend@erlendtermaat.nl>
2022-04-11 13:41:23 +0200

    Take the secret key out of the repository.

---
+++ project/djocker/local_secret_key.example.py
@@ -0,0 +1,8 @@
+# Copy this file to add_secret_key.py, and create a secret key for the site.
+LOCAL_SECRET_KEY = ...
+
+# Example on how to generate the key:
+#
+# from django.core.management.utils import get_random_secret_key
+# # print new random secret key
+# print(get_random_secret_key())
--- project/home/tasks.py
+++ project/home/tasks.py
@@ -1,11 +1,16 @@
+import logging
+
 from celery import shared_task
 from djocker.celery import app
 from home.models import BackendLog
+
+logger = logging.getLogger(__name__)
 
 
 @app.task
 def run_task(argument):
     BackendLog.objects.create(message=str(argument))
+    logger.info("Getting at the task with {}".format(argument))
     return argument
 
 
